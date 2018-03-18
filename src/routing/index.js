50b7441f342ac09264d96b7f5d469855779aee7b
Nicklas Laine Overgaard <nicklas@skarp.dk>
2018-03-18 12:29:31 +0100

    Add hasRoute and "routing" module to library

---
+++ src/routing/index.js
@@ -0,0 +1,5 @@
+const hasRoute = require('./hasRoute');
+
+module.exports = {
+  hasRoute
+};
---
+++ src/routing/routing.spec.js
@@ -0,0 +1,39 @@
+const Hapi = require('hapi');
+
+const hasRoute = require('./hasRoute');
+
+describe('routing', () => {
+  describe('hasRoute helper', () => {
+    let server;
+    beforeEach(async () => {
+      server = new Hapi.Server({});
+
+      server.route({
+        path: '/hello',
+        method: 'get',
+        options: {
+          handler: () => {
+            return 'hello'
+          }
+        }
+      });
+    });
+
+    it('returns true when route exist', () => {
+      expect(hasRoute(server, '/hello', 'get')).toBeTruthy();
+    });
+
+    it('returns false when route does not exist', () => {
+      expect(hasRoute(server,'/not-there', 'get')).toBeFalsy();
+    });
+
+    it('returns false when route is there but method invalid', () => {
+      expect(hasRoute(server, '/hello', 'post')).toBeFalsy();
+    });
+
+    it('works when no routes are registered', () => {
+      const otherServer = new Hapi.Server({});
+      expect(hasRoute(otherServer, '/something', 'get')).toBeFalsy();
+    });
+  });
+});
